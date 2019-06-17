0dd39d941e4c8bc0ea8f8f2dd04c01e49bcc1dcc
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-17 10:14:05 -0400

    finished stringArrayUtils

--- src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
@@ -10,8 +10,15 @@ public class StringArrayUtils {
      * @param endingIndex - ending index of array
      * @return an array with all elements between `startingIndex` and `endingIndex`
      */
-    public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) {
-        return null;
+    public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex)
+            throws IndexOutOfBoundsException, IllegalArgumentException {
+        if (startingIndex < 0 | endingIndex < 0 ) throw new IllegalArgumentException();
+        if (startingIndex >= arrayToBeSpliced.length | endingIndex > arrayToBeSpliced.length) throw new IndexOutOfBoundsException();
+        String[] result = new String[(endingIndex - startingIndex)];
+        for (int i = startingIndex; i < endingIndex; i++)
+            result[i-startingIndex] = arrayToBeSpliced[i];
+
+        return result;
     }
 
 
@@ -20,7 +27,10 @@ public class StringArrayUtils {
      * @param startingIndex - starting index of array to be spliced
      * @return an array all elements between after `startingIndex`
      */
-    public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex) {
-        return null;
+    public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex)
+            throws IndexOutOfBoundsException, IllegalArgumentException {
+        if (startingIndex < 0) throw new IndexOutOfBoundsException();
+        if (startingIndex >= arrayToBeSpliced.length) throw new IllegalArgumentException();
+        return getSubArray(arrayToBeSpliced, startingIndex, arrayToBeSpliced.length);
     }
 }
