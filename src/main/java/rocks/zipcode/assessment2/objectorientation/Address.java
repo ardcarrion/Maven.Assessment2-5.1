9eacf3cba331c066a3c298280ed23077c3ee6f07
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-17 11:21:51 -0400

    Address and Person done

--- src/main/java/rocks/zipcode/assessment2/objectorientation/Address.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Address.java
@@ -4,7 +4,21 @@ package rocks.zipcode.assessment2.objectorientation;
  * @author leon on 28/11/2018.
  */
 public class Address {
+
+
+
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String zipcode;
+
     public Address() {
+        this.addressLine1 = "";
+        this.addressLine2 = "";
+        this.city = "";
+        this.state = "";
+        this.zipcode = "";
     }
 
     /**
@@ -15,45 +29,72 @@ public class Address {
      * @param zipcode - zipcode of region
      */
     public Address(String addressLine1, String addressLine2, String city, String state, String zipcode) {
-    }
-
-    public String getAddressLine1() {
-        return null;
-    }
-
-    public void setAddressLine1(String addressLine1) {
-    }
-
-    public String getAddressLine2() {
-        return null;
-    }
-
-    public void setAddressLine2(String addressLine2) {
-    }
-
-    public String getCity() {
-        return null;
-    }
-
-    public void setCity(String city) {
-    }
-
-    public String getState() {
-        return null;
-    }
-
-    public void setState(String state) {
-    }
-
-    public String getZipcode() {
-        return null;
-    }
-
-    public void setZipcode(String zipcode) {
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
     }
 
     @Override
     public boolean equals(Object o) {
-        return (Boolean)null;
+        Address compare = (Address)o;
+        if (!compare.addressLine1.equals(this.addressLine1)) return false;
+        if (!compare.addressLine2.equals(this.addressLine2)) return false;
+        if (!compare.city.equals(this.city)) return false;
+        if (!compare.state.equals(this.state)) return false;
+        if (!compare.zipcode.equals(this.zipcode)) return false;
+        return true;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "addressLine1='" + addressLine1 + '\'' +
+                ", addressLine2='" + addressLine2 + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipcode='" + zipcode + '\'' +
+                '}';
     }
 }
