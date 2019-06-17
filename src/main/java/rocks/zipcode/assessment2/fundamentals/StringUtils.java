3dfc2d6aaa51550e6dc65ed44adeac7767dc80b8
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-17 09:53:51 -0400

    finished stringutils

--- src/main/java/rocks/zipcode/assessment2/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/assessment2/fundamentals/StringUtils.java
@@ -10,7 +10,9 @@ public class StringUtils {
      * @return `stringToBePadded` flushed right by left-padding
      */
     public static String padLeft(String stringToBePadded, int amountOfPadding) {
-        return null;
+        StringBuilder padded = new StringBuilder(stringToBePadded);
+        for (int i = 0; i < amountOfPadding - stringToBePadded.length(); i++) padded.insert(0, " ");
+        return padded.toString();
     }
 
     /**
@@ -19,7 +21,9 @@ public class StringUtils {
      * @return `stringToBePadded` flushed right by right-padding
      */
     public static String padRight(String stringToBePadded, int amountOfPadding) {
-        return null;
+        StringBuilder padded = new StringBuilder(stringToBePadded);
+        for (int i = 0; i < amountOfPadding - stringToBePadded.length(); i++) padded.insert(stringToBePadded.length(), " ");
+        return padded.toString();
     }
 
     /**
@@ -28,7 +32,9 @@ public class StringUtils {
      * @return the string repeated and concatenated `n` times
      */
     public static String repeatString(String stringToBeRepeated, int numberOfTimeToRepeat) {
-        return null;
+        StringBuilder repeated = new StringBuilder();
+        for (int i = 0; i < numberOfTimeToRepeat; i++) repeated.append(stringToBeRepeated);
+        return repeated.toString();
     }
 
     /**
@@ -36,7 +42,12 @@ public class StringUtils {
      * @return - true if string only contains alpha characters
      */
     public static Boolean isAlphaString(String string) {
-        return null;
+
+        char[] chars = string.toLowerCase().toCharArray();
+        for (char c : chars) {
+            if ((c < 97 | c > 122) && c != 32) return false;
+        }
+        return true;
     }
 
     /**
@@ -44,7 +55,9 @@ public class StringUtils {
      * @return - true if string only contains numeric characters
      */
     public static Boolean isNumericString(String string) {
-        return null;
+        char[] chars = string.toCharArray();
+        for (char c : chars) if (!(c >= 48 && c <= 57)) return false;
+        return true;
     }
 
     /**
@@ -52,6 +65,11 @@ public class StringUtils {
      * @return - true if string only contains special characters
      */
     public static Boolean isSpecialCharacterString(String string) {
-        return null;
+        char[] chars = string.toCharArray();
+        for (char c : chars) {
+            String letter = String.valueOf(c);
+            if (isAlphaString(letter) | isNumericString(letter)) return false;
+        }
+        return true;
     }
 }
