1d4dd4226ee1a5517bd8c42fe2c59555bd9a30c1
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-17 11:05:21 -0400

    1 failing test in mapconversion

--- src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
+++ src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
@@ -1,23 +1,36 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Use a map to solve
  */
 public class MonthConversion {
+    private HashMap<Integer, String> monthsByNumber;
+    private HashMap<String, Integer> monthsByName;
+
+    public MonthConversion() {
+        this.monthsByNumber = new HashMap<>();
+        this.monthsByName = new HashMap<>();
+    }
+
     /**
      * @param monthNumber - ordinal of month in the year; i.e. January = 1, February = 2
      * @param monthName - name of month
      */
     public void add(Integer monthNumber, String monthName) {
-
+        monthsByNumber.put(monthNumber, monthName);
+        monthsByName.put(monthName, monthNumber);
     }
 
     /**
      * @param monthNumber - ordinal of month in the year
      * @return the name of the respective month
      */
-    public String getName(Integer monthNumber) {
-        throw new NullPointerException();
+    public String getName(Integer monthNumber)  {
+        return monthsByNumber.get(monthNumber);
     }
 
     /**
@@ -25,7 +38,7 @@ public class MonthConversion {
      * @return - the ordinal of the month in the year
      */
     public int getNumber(String monthName) {
-        return (Integer)null;
+        return monthsByName.getOrDefault(monthName, null);
     }
 
     /**
@@ -33,7 +46,9 @@ public class MonthConversion {
      * @return true if the monthNumber is in the keySet
      */
     public Boolean isValidNumber(Integer monthNumber) {
-        return null;
+        Set<Integer> months = monthsByNumber.keySet();
+        for (Integer month : months) if (monthNumber.equals(month)) return true;
+        return false;
     }
 
     /**
@@ -41,14 +56,16 @@ public class MonthConversion {
      * @return true if the monthName is in the valueSet
      */
     public Boolean isValidMonth(String monthName) {
-        return null;
+        Set<String> months = monthsByName.keySet();
+        for (String month : months) if (monthName.equals(month)) return true;
+        return false;
     }
 
     /**
      * @return number of entries in this mapping
      */
     public Integer size() {
-        return -1;
+        return monthsByNumber.size();
     }
 
     /**
@@ -56,6 +73,7 @@ public class MonthConversion {
      * @param monthName - name of month
      */
     public void update(Integer monthNumber, String monthName) {
-
+        monthsByName.replace(monthName, monthNumber);
+        monthsByNumber.replace(monthNumber, monthName);
     }
 }
